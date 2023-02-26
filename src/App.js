e49678ebe8db1d9cc663488e37b020396ab0a62c
khairmahjar <khairmahjar@gmail.com>
2023-02-26 14:50:55 +0700

    adding new text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const App = () => {
   return (
-    <h1>Ini React Cuy. Ini Branch Navbar Cuy</h1>
+    <>
+      <h1>Ini React Cuy. Ini Branch Navbar Cuy</h1>
+      <h1>2 Cuy</h1>
+      <h1>3 Cuy</h1>
+    </>
+    
   );
 }
 
